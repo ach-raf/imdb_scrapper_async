6a39783b1eef70f02277d831df0cdbf00e1b8a92
achraf.zouchou <achraf.zouchou@gmail.com>
2022-04-07 14:19:27 +0000

    added a link on how setuptools works

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+# https://stackoverflow.com/questions/6323860/sibling-package-imports/50193944#50193944
+
 from setuptools import setup, find_packages
 
 setup(name="imdb_scrapper", version="1.0", packages=find_packages())
